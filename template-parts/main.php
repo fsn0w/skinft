fb8f0a29fe97524b2d15f19986a9de6604b88504
Kyryll Prytula <k.prytula@gmail.com>
2022-02-19 17:30:39 +0200

    upd

--- template-parts/main.php
+++ template-parts/main.php
@@ -14,11 +14,13 @@ get_header();
 <div class="flex pt-52 pb-10 bg-title">
 
     <div class="container mx-auto flex">
-        <div class="w-3/5">
+        <div class="w-3/5 text-3xl font-thin">
             <h1>an nft collection you can play with</h1>
-            <p>The first ever NFT you can use in-game from the get go.</p>
-            <p>SkinFT is an NFT skin collection designed for <span class="minecraft">MineCraft</span></p>
-            <a href="" class="text-4xl text-white bg-primary p-3 rounded font-normal">Mint a SkinFT</a>
+            <div class=" w-3/4">
+                <p>The first ever NFT you can use in-game from the get go.</p>
+                <p>SkinFT is an NFT skin collection designed for <span class="minecraft">MineCraft</span></p>
+            </div>
+            <a href="" class="mt-10 inline-block text-4xl text-white bg-primary p-3 rounded font-normal">Mint a SkinFT</a>
         </div>
         <div class="w-2/5">
             img
