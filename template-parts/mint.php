7a586af54425925eb9c08cf1d6d0b9b720e6ec15
Kyryll Prytula <k.prytula@gmail.com>
2022-03-10 23:10:44 +0200

    mint counter

--- template-parts/mint.php
+++ template-parts/mint.php
@@ -5,6 +5,13 @@
  */
 
 get_header();
+
+$start = 'Mar 28, 2022 08:00:00';
+
+$cdate = strtotime(current_time("M j, Y H:i:s"));
+$sdate = strtotime($start);
+$mint = ((isset($_GET['timeroff']) || $cdate>$sdate)?true:false);
+
 ?>
 
 <div id="content" class="relative site-content text-light">
@@ -13,7 +20,10 @@ get_header();
 
 <div class="w-full pt-32 lg:pt-52 pb-32 bg-title text-center">
 
-            <div class="inline-block mx-auto text-center bg-[#f7f0e834] p-6 lg:px-32 rounded relative">
+            <div class="inline-block mx-auto text-center bg-[#f7f0e834] p-6 rounded relative lg:min-w-[630px]">
+<?php 
+    if ($mint) { 
+?>
                 <h2 class="text-center text-light mb-6 lg:mb-16 text-lg lg:block"><span>M</span>int your <span>S</span>kin<span>FT</span></h2> 
                 <div class="whitespace-nowrap text-light text-5xl text-center flex flex-row justify-center items-center my-4 lg:text-5xl">
                     <button class="minus border-[3px] border-light rounded-xl font-light w-12 h-12 leading-[1px] mt-2 mr-6 lg:mt-4 lg:mx-8 lg:w-12 lg:h-12">&#8211;</button>
@@ -28,14 +38,67 @@ get_header();
                     <button id="mintbtn" class="inline-block text-xl lg:text-4xl text-white bg-primary p-3 rounded font-normal hover:bg-[#56952b] mr-2 whitespace-nowrap">
                         <span class="hidden lg:inline">Mint</span><span class="lg:hidden">Mint a SkinFT</span></button>
                 </div>
-                <img src="<?=get_stylesheet_directory_uri()?>/assets/img/mint-3d-model-front.png" class="absolute h-32 w-auto -left-4 -bottom-24 lg:-left-60 lg:top-0" />
+                <img src="<?=get_stylesheet_directory_uri()?>/assets/img/mint-3d-model-front.png" class="absolute h-32 w-auto -left-4 -bottom-24 lg:-left-60 lg:top-0 lg:h-auto " />
                 <img src="<?=get_stylesheet_directory_uri()?>/assets/img/mint-3d-model-back.png" class="hidden absolute -right-60 -bottom-16 lg:inline-block" />
+<?php
+    } else {
+?>
+                <h2 class="text-center text-light mb-6 lg:mb-16 text-lg lg:block">
+                    <span>S</span>kin<span>FT</span>s are <span>C</span>oming <span>!</span>
+                </h2> 
+                <span class="block mx-auto text-counter font-medium mt-4 lg:mx-10 lg:mt-7 lg:text-2xl">
+                            <ul class="flex justify-between gap-3" id="mintcounter">
+                                <li><div id="days"></div><span>Days</span></li>
+                                <li><div id="hours"></div><span>Hours</span></li>
+                                <li><div id="minutes"></div><span>Minutes</span></li>
+                                <li><div id="seconds"></div><span>Seconds</span></li>
+                            </ul>
+                </span>
+                <p class="font-medium text-xl lg:text-4xl">See you on Monday, March 28,<br> at 8:00AM EST</p>
+                <img src="<?=get_stylesheet_directory_uri()?>/assets/img/mint-3d-model-front.png" class="absolute h-32 w-auto left-4 -bottom-24 lg:hidden" />
+<?php 
+    }
+?>
             </div>
 </div>
 
 <!-- /Mint Section -->
 
 </div>
+
+<script>
+(function () {
+      
+      let countUpDate = new Date("<?=$start?>").getTime();
+      let x = setInterval(function() {    
+
+      var now = new Date().getTime();
+      var distance = countUpDate - now;
+
+        if (distance < 0) {
+
+          document.getElementById("days").innerText = "0",
+          document.getElementById("hours").innerText = "0",
+          document.getElementById("minutes").innerText = "0",
+          document.getElementById("seconds").innerText = "0";
+
+          clearInterval(x);
+        }
+        else {
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById("days").innerText = days,
+            document.getElementById("hours").innerText = hours,
+            document.getElementById("minutes").innerText = minutes,
+            document.getElementById("seconds").innerText = seconds;
+        }
+      }, 0)
+
+  }());
+    </script>
 
 <?php
 
